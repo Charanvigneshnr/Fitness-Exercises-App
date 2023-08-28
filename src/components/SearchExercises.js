742a6649823295e05cf866c9896cc1f90541fb00
Charan Vignesh N R <charanvignesh10@gmail.com>
2023-08-28 16:31:59 +0530

    connecting with rapidapi

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 const SearchExercises = () => {
+  const [search, setSearch] = useState("");
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData();
+    }
+  };
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
       <Typography
@@ -17,20 +23,37 @@ const SearchExercises = () => {
         <TextField
           sx={{
             input: { fontWeight: "700", border: "none", borderRadius: "4px" },
-            width: { lg: "1170px", xs: "350px" },
+            width: { lg: "700px", xs: "350px" },
             backgroundColor: "#FFFFFF",
             borderRadius: "40px",
           }}
           height="76px"
-          value=""
-          onChange={(e) => {}}
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value.toLowerCase());
+          }}
           placeholder="Search Exercises"
           type="text"
-        ></TextField>
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#FF2625",
+            color: "#FFFFFF",
+            textTransform: "none",
+            width: { lg: "120px", xs: "70px" },
+            fontSize: { lg: "20px", xs: "14px" },
+            height: "56px",
+            position: "absolute",
+            right: "0",
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
       </Box>
     </Stack>
   );
 };
 
 export default SearchExercises;
-//41:47
